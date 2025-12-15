34c46d0ea3d4d47e84a87ba8018e786a8c029d81
hitesh-repo <tohiteshsharma@gmail.com>
2025-12-15 23:12:17 +0530

    commit email settings

--- api/config.php
+++ api/config.php
@@ -1,8 +1,8 @@
 <?php
 define("RECAPTCHA_SECRET", "6Ldc9-YrAAAAAA09FaRcduF5PQSvFMhAEvHfrSMQ");
 define("SMTP_HOST", "smtp.gmail.com");
-define("SMTP_USER", "youradmin@gmail.com");
-define("SMTP_PASS", "GMAIL_APP_PASSWORD");
+define("SMTP_USER", "codetrio2025@gmail.com");
+define("SMTP_PASS", "wabx badx dgjf gpks");
 define("SMTP_PORT", 587);
 
 define("ADMIN_EMAIL", "codetrio2025@gmail.com");
