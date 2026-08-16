8a09090e2b11d2c6617c1e633e4daca921e22ae2
hitesh-repo <tohiteshsharma@gmail.com>
2026-08-16 23:16:51 +0530

    commit

--- frontend/src/Components/Pages/ProtfolioPage.jsx
+++ frontend/src/Components/Pages/ProtfolioPage.jsx
@@ -1,28 +1,27 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import Style from '../CSS/Style.module.css';
-import AboutBanner from '../../assets/images/about-banner.webp';
-import { Link } from 'react-router-dom';
+import Style from "../CSS/Style.module.css";
+import AboutBanner from "../../assets/images/about-banner.webp";
+import { Link } from "react-router-dom";
 //Icon
-import SEO from '../Common/webSiteMeta.jsx';
-import CATComponent from './CATComponent.jsx';
+import SEO from "../Common/webSiteMeta.jsx";
+import CATComponent from "./CATComponent.jsx";
 //Components
-import OurPortfolio from '../HomeRoute/Portfolio';
-import VisionComponent from './VisionCode';
-const PortfolioPage = ()=>{
-
-    return(
-        <>
-        <SEO page="portfolio" />
-        <div className={Style.innerPage + " " + Style.PortfolioPage}>
-            <div className={Style.innerBanner}>
-                <img src={AboutBanner} />
-            </div>
-            <OurPortfolio/>
+import OurPortfolio from "../HomeRoute/Portfolio";
+import VisionComponent from "./VisionCode";
+const PortfolioPage = () => {
+  return (
+    <>
+      <SEO page="portfolio" />
+      <div className={Style.innerPage + " " + Style.PortfolioPage}>
+        <div className={Style.innerBanner}>
+          <img src={AboutBanner} />
         </div>
-        <CATComponent />
-        </>
-    )
-}
+        <OurPortfolio />
+      </div>
+      <CATComponent />
+    </>
+  );
+};
 
 export default PortfolioPage;
